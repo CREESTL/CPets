da70b1240db87b7b00078cf76f5cdf592a4a8a09
CREESTL <creestl.main@yandex.ru>
2023-11-16 23:47:35 +0300

    Finish ex11_13. That was tough!

--- exercises/ch11/ex11_13.c
+++ exercises/ch11/ex11_13.c
@@ -12,13 +12,18 @@
     9: X Y Z
 
     So some people try to remember phone numbers as words.
-    For example: 825-3688 means "TAKEOUT". A good number for a restaurant.
+    Examples:
+    - 8253688 : TAKEOUT
+    - 4247288 : HAIRCUT
+    - 2337226 : BEERCAN
+    - 7382273 : PETCARE
 
     Find all possible works for a given 7-digit number and write them into a file
 
     There are 2187 (3^7) possible words for each 7-digit number
 */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -27,6 +32,9 @@
 /* Length of string representing a digit */
 #define LETTERS_PER_DIGIT 3
 
+/* File to output found words to */
+#define output_file "exercises/ch11/words.dat"
+
 /* Map from digits to letters */
 char digits[NUM_LENGTH] = {'2', '3', '4', '5', '6', '7', '8', '9'};
 char *letters[NUM_LENGTH] = {"ABC", "DEF", "GHI", "JKL", "MNO", "PRS", "TUV", "XYZ"};
@@ -34,12 +42,36 @@ char *letters[NUM_LENGTH] = {"ABC", "DEF", "GHI", "JKL", "MNO", "PRS", "TUV", "X
 /* Positions of printed letters in matrix */
 int pos_in_lines[NUM_LENGTH - 1] = {0};
 
+/* Check that user num only contains allowed digits */
+bool validate_user_num(const char *const num)
+{
+    bool in_list = false;
+    for (int i = 0; i < NUM_LENGTH; i++)
+    {
+        for (int j = 0; j < NUM_LENGTH; j++)
+        {
+            if (num[i] == digits[j])
+            {
+                in_list = true;
+            }
+        }
+    }
+    return in_list;
+}
+
+/* Get telephone number from user */
 void get_user_num(char *num)
 {
     puts("Input a 7-digit number: ");
     fgets(num, NUM_LENGTH + 1, stdin);
+    if (!validate_user_num(num))
+    {
+        puts("Invalid digits!");
+        exit(EXIT_FAILURE);
+    }
 }
 
+/* Form matrix of chars representing digits */
 void form_matrix(const char *const num, char const digits[NUM_LENGTH], char *const letters[NUM_LENGTH],
                  char matrix[NUM_LENGTH][LETTERS_PER_DIGIT])
 {
@@ -58,17 +90,6 @@ void form_matrix(const char *const num, char const digits[NUM_LENGTH], char *con
             }
         }
     }
-
-    puts("MATRIX IS: ");
-    for (int i = 0; i < NUM_LENGTH; i++)
-    {
-        for (int j = 0; j < LETTERS_PER_DIGIT; j++)
-        {
-            printf("%c ", matrix[i][j]);
-        }
-        puts("");
-    }
-    puts("============");
 }
 
 /* Move to the next num in upper line if the end of lower line reached */
@@ -90,16 +111,16 @@ void check_above(int pos_in_lines[NUM_LENGTH - 1])
 void check_all_and_reset(int pos_in_lines[NUM_LENGTH - 1])
 {
     int start = 0;
-    int change = 0;
+    bool change = false;
     for (int line = 0; line < NUM_LENGTH - 1; line++)
     {
         if (pos_in_lines[line] > LETTERS_PER_DIGIT - 1 && line > 0)
         {
             /* Remember the line and allow to change */
             start = line;
-            change = 1;
+            change = true;
         }
-        if (change == 1 && line >= start)
+        if (change && line >= start)
         {
             /* Reset lines below */
             pos_in_lines[line] = 0;
@@ -107,23 +128,20 @@ void check_all_and_reset(int pos_in_lines[NUM_LENGTH - 1])
     }
 }
 
+/* Find all possible combinations of characters from matrix */
 void find_words(char matrix[NUM_LENGTH][LETTERS_PER_DIGIT])
 {
+
+    FILE *output = fopen(output_file, "w");
+    if (output == NULL)
+    {
+        puts("Failed to open output file!");
+        exit(EXIT_FAILURE);
+    }
 
     int count = 0;
     while (1)
     {
-        /**
-         * 
-         * DEF
-         * DEF
-         * DEF
-         * GHI
-         * GHI
-         * GHI
-         * ABC 
-         * 
-        */
 
         check_above(pos_in_lines);
         check_all_and_reset(pos_in_lines);
@@ -134,19 +152,18 @@ void find_words(char matrix[NUM_LENGTH][LETTERS_PER_DIGIT])
 
         for (int line = 0; line < NUM_LENGTH - 1; line++)
         {
-            printf("%c ", matrix[line][pos_in_lines[line]]);
+            fprintf(output, "%c", matrix[line][pos_in_lines[line]]);
             if (line == NUM_LENGTH - 2)
             {
                 /* Each run num from last line changes */
                 pos_in_lines[line]++;
             }
         }
-        puts("");
+        fputs("\n", output);
         count++;
-        
     }
-
-    printf("Total number: %d\n", count);
+    fclose(output);
+    printf("%d words have been found and written into file.\n", count);
 }
 
 int main(void)
